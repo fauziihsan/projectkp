ce049aa95a5e612a10c1f8f5b4592b332b090246
Fauzi Ihsan <fauzyihsan@gmail.com>
2020-10-16 09:00:26 +0700

    membuat controller dan model untuk auth dashboard

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -1,12 +1,18 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class User extends CI_Controller
+class Dashboard extends CI_Controller
 {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('dashboard_model', 'dashboard');
+    }
+
     public function index()
     {
-        // $data = $this->db->get('user')->result_array();
-        // var_dump($data);
+        // $data = $this->dashboard->user();
 
         $this->load->view('templates/header');
         $this->load->view('templates/index');
